378fb4e00cfab3f1d0b2e7e6cff379ff50f1153b
Andrew Culver <culver.andrew@gmail.com>
2011-03-09 15:38:15 -0600

    Forgot to add source changes.

--- qotd.py
+++ qotd.py
@@ -1,64 +1,68 @@
-#!/usr/bin/python
-
+#!/usr/local/bin/python2.7
+import csv
 import socket
+import sys
 
-HOST = "localhost"
-PORT = 17
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-s.bind((HOST, PORT))
-s.settimeout(5.0)
-s.listen(5)
-print "QOTD server listening on {}:{}. Press CTRL+C to exit..\n".format(HOST, PORT)
 
 class Quote:
 	"""Generic Quote"""
-	def __init__(self, msg, author="Unknown"):
-		self.msg = msg
+	def __init__(self, quote, author):
+		self.quote = quote
 		self.author = author
 	
 	def __repr__(self):
-		return "\"{}\"\n\n-{}\n".format(self.msg, self.author)
+		return "\"{}\"\n\n-{}\n".format(self.quote, self.author)
 
 
-# Gross hardcoded quotes.
-q1 = """They're the best at what they do and I'm the best at what I do.
-And together it's like, it's on. Sorry, Middle America. Yeah, I said it."""
+def load_csv_quotes(path):
+	"""Load quotes from csv. Returns an array of quotes."""
+	
+	try:
+		f = open(path, 'r')
+		reader = csv.DictReader(f, ('author', 'quote'), delimiter="|")
+		
+	except IOError, e:
+		sys.exit("Unable to open file: {}.".format(e.message))
+	
+	except csv.Error, e:
+		sys.exit(e.message)
+	
+	return [Quote(**line) for line in reader]
+	
+	
 
-q2 = """I'm so tired of pretending like my life isn't just perfect
-and just winning every second, and I'm not just perfect
-and bi**hing and just delivering the goods at every frickin' turn."""
-
-q3 = """I have a 10,000-year-old brain and the boogers of a 7-year-old.
-That's how I describe myself."""
-
-author = "Charlie Sheen"
-
-quotes = []
-quotes.append(Quote(q1, author))
-quotes.append(Quote(q2, author))
-quotes.append(Quote(q3, author))				
-
-
-def get_quote():
+def quote_cycle(quotes):
 	"""Generator to cycle through quotes."""
 	while 1:
 		for q in quotes:
 			yield str(q)
 
-quote_list = get_quote()
+if __name__ == "__main__":
+	
+	HOST = "localhost"
+	PORT = 17
+	FILEPATH = "quotes.txt"
 
-while True:
-	try:
-		conn, addr = s.accept()
-		print addr
-		conn.send(quote_list.next())
-		conn.close()
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	s.bind((HOST, PORT))
+	s.settimeout(5.0)
+	s.listen(5)
+	print "QOTD server listening on {}:{}. Press CTRL+C to exit..\n".format(HOST, PORT)
+	
+	quotes = load_csv_quotes(FILEPATH)
+	cycle = quote_cycle(quotes)
+	
+	while True:
+		try:
+			conn, addr = s.accept()
+			print addr
+			conn.send(cycle.next())
+			conn.close()
 		
-	except KeyboardInterrupt:
-		s.close()
-		break
+		except KeyboardInterrupt:
+			s.close()
+			break
 		
-	except socket.timeout:
-		pass
+		except socket.timeout:
+			pass
