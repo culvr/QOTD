6066ca97fd12237428cd9bd93e376c3bfa96e3be
Andrew Culver <culver.andrew@gmail.com>
2011-03-10 08:53:06 -0600

    Refactored server code into a class

--- qotd.py
+++ qotd.py
@@ -5,64 +5,72 @@ import sys
 
 
 class Quote:
-	"""Generic Quote"""
-	def __init__(self, quote, author):
-		self.quote = quote
-		self.author = author
-	
-	def __repr__(self):
-		return "\"{}\"\n\n-{}\n".format(self.quote, self.author)
+    """Generic Quote"""
+    def __init__(self, quote, author):
+        self.quote = quote
+        self.author = author
+    
+    def __repr__(self):
+        return "\"{}\"\n\n-{}\n".format(self.quote, self.author)
 
 
-def load_csv_quotes(path):
-	"""Load quotes from csv. Returns an array of quotes."""
-	
-	try:
-		f = open(path, 'r')
-		reader = csv.DictReader(f, ('author', 'quote'), delimiter="|")
-		
-	except IOError, e:
-		sys.exit("Unable to open file: {}.".format(e.message))
-	
-	except csv.Error, e:
-		sys.exit(e.message)
-	
-	return [Quote(**line) for line in reader]
-	
-	
+class QOTD(object):
+    """Quote of the Day Server"""
+    
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.quotes = self.load_quotes()
 
-def quote_cycle(quotes):
-	"""Generator to cycle through quotes."""
-	while 1:
-		for q in quotes:
-			yield str(q)
+    @property
+    def next_quote(self):
+        return self.quotes.next()
+    
+        
+    def load_quotes(self):
+        """Load quotes from csv. Returns an array of quotes."""
+        
+        try:
+            with open(self.filepath, 'r') as f:
+                reader = csv.DictReader(f, ('author', 'quote'), delimiter="|")
+                quotes = [Quote(**line) for line in reader]
+                
+        except IOError:
+            sys.exit("Could not open file: {}".format(self.filepath))
+                
+        def quote_generator():
+            while 1:
+                for q in quotes:
+                    yield str(q)
+                    
+        return quote_generator()
+    
+    
+    def run(self, host="localhost", port=17):
+        """Server runloop"""
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((host, port))
+        s.settimeout(5.0)
+        s.listen(5)
+        print "QOTD server listening on {}:{}. Press CTRL+C to exit..\n".format(host, port)
+        
+        while True:
+            try:
+                conn, addr = s.accept()
+                print addr
+                conn.send(self.next_quote)
+                conn.close()
+
+            except KeyboardInterrupt:
+                s.close()
+                break
+
+            except socket.timeout:
+                pass
+        
+    
 
 if __name__ == "__main__":
-	
-	HOST = "localhost"
-	PORT = 17
-	FILEPATH = "quotes.txt"
-
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-	s.bind((HOST, PORT))
-	s.settimeout(5.0)
-	s.listen(5)
-	print "QOTD server listening on {}:{}. Press CTRL+C to exit..\n".format(HOST, PORT)
-	
-	quotes = load_csv_quotes(FILEPATH)
-	cycle = quote_cycle(quotes)
-	
-	while True:
-		try:
-			conn, addr = s.accept()
-			print addr
-			conn.send(cycle.next())
-			conn.close()
-		
-		except KeyboardInterrupt:
-			s.close()
-			break
-		
-		except socket.timeout:
-			pass
+    
+    server = QOTD("quotes.txt")
+    server.run()
+    
